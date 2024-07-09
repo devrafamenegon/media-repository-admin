56150e696160ece2b7d524d3112409be9d7f5320
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-09 17:12:57 -0300

    feat: update main page and layout

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Navbar from "@/components/navbar"
+
+export default function MainPage() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-export default async function SetupLayout({
+export default function MainLayout({
   children
 }: {
   children: React.ReactNode;
