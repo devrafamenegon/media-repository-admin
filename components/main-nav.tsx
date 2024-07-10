b9771984b5c05fc55a06b3b8d8e12c3f1beb5162
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-10 17:05:03 -0300

    feat: add nav logo dark and light

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -4,12 +4,26 @@ import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import Image from 'next/image';
+import LogoDark from "@/public/logo_dark.svg";
+import LogoLight from "@/public/logo_light.svg";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
+  const [isMounted, setIsMounted] = useState(false);
+
   const pathname = usePathname();
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, []);
+
+  if (!isMounted) return null;
   
   const routes = [
     {
@@ -18,9 +32,9 @@ export function MainNav({
       active: pathname === `/`
     },
     {
-      href: `/users`,
-      label: 'Users',
-      active: pathname === `/users`
+      href: `/participants`,
+      label: 'Participants',
+      active: pathname === `/participants`
     },
     {
       href: `/medias`,
@@ -31,7 +45,16 @@ export function MainNav({
   
   return (
     <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
-      
+      <Link
+        href={'/'}
+      >
+        <Image 
+          height="20"
+          alt="Logo"
+          src={theme === 'light' ? LogoDark : LogoLight}
+        />
+      </Link>
+
       {routes.map((route) => (
         <Link
           key={route.href}
