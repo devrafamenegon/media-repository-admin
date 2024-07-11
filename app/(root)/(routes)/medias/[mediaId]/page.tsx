eaaa1aa062c9a8c9d7c5e389c97db2c32fb50db7
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-10 21:16:46 -0300

    feat: create media page

---
+++ app/(root)/(routes)/medias/[mediaId]/page.tsx
@@ -0,0 +1,27 @@
+import prismadb from "@/lib/prismadb";
+
+import { MediaForm } from "./components/media-form";
+
+const MediaPage = async ({
+  params
+}: {
+  params: { mediaId: string }
+}) => {
+  const media = await prismadb.media.findUnique({
+    where: {
+      id: params.mediaId
+    }
+  });
+
+  const participants = await prismadb.participant.findMany();
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <MediaForm initialData={media} participants={participants}/>
+      </div>
+    </div>
+  )
+}
+
+export default MediaPage;
