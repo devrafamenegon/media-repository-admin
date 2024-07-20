9ff29c4cc73f8262320399b62a7b325e926410a2
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-20 17:13:23 -0300

    feat: setup cors for all routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS || "false",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              process.env.ACCESS_CONTROL_ALLOW_ORIGIN ||
+              "http://localhost:3001",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value:
+              process.env.ACCESS_CONTROL_ALLOW_METHODS ||
+              "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              process.env.ACCESS_CONTROL_ALLOW_HEADERS ||
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
