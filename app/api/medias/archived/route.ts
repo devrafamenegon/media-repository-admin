073b5a16ac43b6d217810d5ca23598da3d021c45
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-25 23:56:20 -0300

    feat: create get and post archived medias

--- app/api/medias/archived/route.ts
+++ app/api/medias/archived/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
 
 export async function GET(
   req: Request,
@@ -23,6 +24,27 @@ export async function GET(
     return NextResponse.json(medias);
   } catch (error) {
     console.log('[MEDIAS_ARCHIVED_GET]', error);
+    return new NextResponse("Internal error", { status: 500 })
+  }
+}
+
+export async function POST(
+  req: Request,
+) {
+  try {
+    const { password } = await req.json();
+
+    if (!password) {
+      return new NextResponse("Password is required", { status: 400 })
+    }
+
+    if (password.toString() === process.env.ARCHIVED_SECRET?.toString()) {
+      return NextResponse.json({ success: true });
+    }
+
+    return NextResponse.json({ success: false });
+  } catch (error) {
+    console.log('[MEDIAS_ARCHIVED_POST]', error);
     return new NextResponse("Internal error", { status: 500 })
   }
 }
